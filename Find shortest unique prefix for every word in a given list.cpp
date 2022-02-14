77b10c96340be933c14693972b043365da675a6d
Ananya_Gupta <Ananyagp2007@gmail.com>
2022-02-14 13:11:44 +0530

    comit cng

--- Find shortest unique prefix for every word in a given list.cpp
+++ Find shortest unique prefix for every word in a given list.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 vector<string> uniquePrefix(vector<string> &a)
 {
     int size = a.size();
- 
+
     /* create an array to store the results */
     vector<string> res(size);
- 
+
     /* sort the array of strings */
     sort(a.begin(), a.end());
- 
+
     /* compare the first string with its only right
-    neighbor */
+    neighbors */
     int j = 0;
     while (j < min(a[0].length() - 1, a[1].length() - 1))
     {
@@ -23,7 +23,7 @@ vector<string> uniquePrefix(vector<string> &a)
     }
     int ind = 0;
     res[ind++] = a[0].substr(0, j + 1);
- 
+
     /* Store the unique prefix of a[1] from its left neighbor */
     string temp_prefix = a[1].substr(0, j + 1);
     for (int i = 1; i < size - 1; i++)
@@ -38,27 +38,27 @@ vector<string> uniquePrefix(vector<string> &a)
             else
                 break;
         }
- 
+
         string new_prefix = a[i].substr(0, j + 1);
- 
+
         /* compare the new prefix with previous prefix */
         if (temp_prefix.length() > new_prefix.length())
             res[ind++] = temp_prefix;
         else
             res[ind++] = new_prefix;
- 
+
         /* store the prefix of a[i+1] unique from its
         left neighbour */
         temp_prefix = a[i + 1].substr(0, j + 1);
     }
- 
+
     /* compute the unique prefix for the last string
     in sorted array */
     j = 0;
     string sec_last = a[size - 2];
- 
+
     string last = a[size - 1];
- 
+
     while (j < min(sec_last.length() - 1, last.length() - 1))
     {
         if (sec_last[j] == last[j])
@@ -66,8 +66,7 @@ vector<string> uniquePrefix(vector<string> &a)
         else
             break;
     }
- 
+
     res[ind] = last.substr(0, j + 1);
     return res;
 }
- 
